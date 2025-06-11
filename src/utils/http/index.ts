ca43a4c8eb31b9142f22b9e35c4d3f64a732fa7c
yinhao191 <>
2025-06-11 09:20:55 +0800

    axios配置修改

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -3,7 +3,7 @@ import { setupInterceptors } from './interceptors'
 
 export function createAxios(options = {}) {
   const defaultOptions = {
-    baseURL: import.meta.env.VITE_AXIOS_BASE_URL,
+    // baseURL: import.meta.env.VITE_AXIOS_BASE_URL,
     timeout: 12000,
   }
   const service = axios.create({
@@ -15,10 +15,12 @@ export function createAxios(options = {}) {
 }
 
 export const request = createAxios({
-  baseURL: '/mock-api', // mock配置
+  baseURL: import.meta.env.VITE_AXIOS_BASE_URL, // dev: https://m1.apifoxmock.com/m1/6094781-5785319-default，
+  url: '/mock-api', // mock配置
 })
 
 // 调用实际后端服务器地址，用这个方法
 export const RealRequest = createAxios({
-  baseURL: '/api',
+  baseURL: 'http://8.137.36.56:8000', // 实际后端服务器地址
+  url: '/api',
 })
